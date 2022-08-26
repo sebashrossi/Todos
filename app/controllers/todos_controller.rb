e753cd67ab64903a053a31351311a7e2171721fb
sebas.rossi <sebas.rossi@sage.com>
2022-08-26 14:37:56 +0200

    Added controller, specs and views for Todos

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,49 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: %i[ show edit update destroy ]
+
+  def index
+    @todos = Todo.all
+  end
+
+  def show; end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def edit; end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      redirect_to todo_url(@todo), notice: "Todo was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todo_url(@todo), notice: "Todo was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_url, notice: "Todo was successfully destroyed."
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+
+  def todo_params
+    params.require(:todo).permit(:name, :status, :user_id)
+  end
+end
---
+++ spec/factories/todos.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :todo do
+    name { "MyString" }
+    status { 1 }
+    user { nil }
+  end
+end
