7f26205cd691ab9c0fcd9982e974e0e429449bed
sebas.rossi <sebas.rossi@sage.com>
2022-08-26 14:37:29 +0200

    added Todo model

---
+++ db/migrate/20220826121954_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.string :name
+      t.integer :status
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
