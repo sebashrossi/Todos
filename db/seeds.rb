0f508dc670b7d8c9c86eff26b5426e45fec0be4a
sebas.rossi <sebas.rossi@sage.com>
2022-08-28 13:44:20 +0200

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Cleaning database..."
+User.destroy_all
+
+
+puts ""
+puts "Creating users..."
+puts ""
+10.times do
+  user = User.new(name: "#{Faker::Name.unique.first_name} #{Faker::Name.unique.last_name}")
+  user.email = Faker::Internet.email(name: user.name, domain: 'netenders.com')
+  user.save!
+end
+
+puts ""
+puts "Creating Todos..."
+puts ""
+status = ['to_do', 'doing', 'done']
+40.times do
+  Todo.create(name: Faker::Hobby.activity, status: status.sample, user_id: User.all.sample.id)
+end
+
+puts ""
+puts "Created: "
+puts ""
+puts "Users: #{User.count}"
+puts "Todos: #{Todo.count}"
